e41b3b964ade7548ffcc8e048f2edd14085bef82
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:43:06 -0700

    Book entity

---
+++ src/main/java/com/test/librarymanagement/domain/data/Book.java
@@ -0,0 +1,42 @@
+package com.test.librarymanagement.domain.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String author;
+
+    private String publicationYear;
+
+    private String ISBN;
+
+    private Integer edition;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedAt;
+}
