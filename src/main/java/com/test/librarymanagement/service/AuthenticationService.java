00f3ecb1bfdf639d00d6bd949bd8fd330da53b20
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:42:26 -0700

    AuthenticationService class

---
+++ src/main/java/com/test/librarymanagement/service/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.test.librarymanagement.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthenticationService {
+}
