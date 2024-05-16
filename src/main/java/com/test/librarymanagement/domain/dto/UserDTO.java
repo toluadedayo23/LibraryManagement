8d3ab7f6bf5b4ac2cbdfb8c6b0a75624d84895a0
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 22:12:35 -0700

    user dto class

---
+++ src/main/java/com/test/librarymanagement/domain/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.test.librarymanagement.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.test.librarymanagement.domain.enums.Role;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserDTO {
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private Boolean enabled;
+
+    private Role role;
+
+    private String address;
+
+}
