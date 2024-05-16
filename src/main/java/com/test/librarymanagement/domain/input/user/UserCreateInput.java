3aa3715baa70dd5a64fa63b4626d968316acbc80
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 21:00:54 -0700

    user create input

---
+++ src/main/java/com/test/librarymanagement/domain/input/user/UserCreateInput.java
@@ -0,0 +1,11 @@
+package com.test.librarymanagement.domain.input.user;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class UserCreateInput {
+}
