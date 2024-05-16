0d806112ad18ac6f20cbe9c792bc577c963d443d
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 22:13:04 -0700

    user update input class

--- src/main/java/com/test/librarymanagement/domain/input/user/UserUpdateInput.java
+++ src/main/java/com/test/librarymanagement/domain/input/user/UserUpdateInput.java
@@ -1,4 +1,15 @@
 package com.test.librarymanagement.domain.input.user;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
 public class UserUpdateInput {
+
+    private String username;
+
+    private String email;
 }
