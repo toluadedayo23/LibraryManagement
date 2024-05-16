41970b2b2a76b0b8920babc9b96a0b9b1c165193
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:47:15 -0700

    exception constant

---
+++ src/main/java/com/test/librarymanagement/constant/ExceptionConstant.java
@@ -0,0 +1,14 @@
+package com.test.librarymanagement.constant;
+
+public interface ExceptionConstant {
+    String ILLEGAL_ARGUMENT_EXCEPTION_MESSAGE = "Your request parameter did not match our schema";
+    String VALIDATION_EXCEPTION_MESSAGE = "Validation exception";
+    String AUTHENTICATION_EXCEPTION_MESSAGE = "Authentication exception";
+    String LIBRARY_MANAGEMENT_VALIDATION_EXCEPTION_MESSAGE = "Validation exception";
+    String LIBRARY_MANAGEMENT_EXCEPTION_MESSAGE = "LibraryManagement exception";
+    String NO_SUCH_ELEMENT_EXCEPTION_MESSAGE = "No Such Element Exception";
+    String THROWABLE_MESSAGE = "Something has gone wrong on the server";
+    String UNABLE_TO_PERFORM_OPERATION = "Unable to perform operation";
+    String PERMISSION_DENIED = "User does not have the required permission to perform operation";
+
+}
