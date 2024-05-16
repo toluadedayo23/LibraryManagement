ef09bd188066c7415164258dc861e587530a18b6
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:45:24 -0700

    User Repository class

---
+++ src/main/java/com/test/librarymanagement/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.test.librarymanagement.repository;
+
+import com.test.librarymanagement.domain.data.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+}
