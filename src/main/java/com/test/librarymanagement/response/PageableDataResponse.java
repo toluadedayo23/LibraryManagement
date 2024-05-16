ca07cd4f6d3e10f260e9587e164e8167c71d7b35
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:53:57 -0700

    page dto

---
+++ src/main/java/com/test/librarymanagement/response/PageableDataResponse.java
@@ -0,0 +1,23 @@
+package com.test.librarymanagement.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.test.librarymanagement.domain.dto.PageableDTO;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+public class PageableDataResponse<T> extends PageableDTO<T> implements Response {
+    private String message;
+
+    public PageableDataResponse(PageableDTO<T> page) {
+        setContent(page.getContent());
+        setTotalPages(page.getTotalPages());
+        setTotalElements(page.getTotalElements());
+        setFirst(page.isFirst());
+        setLast(page.isLast());
+        setPageNumber(page.getPageNumber());
+    }
+
+}
