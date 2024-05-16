c25816ddba045f4896ad514390e295d6ef0a1e30
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 22:11:49 -0700

    patron update class

--- src/main/java/com/test/librarymanagement/domain/input/patron/PatronUpdateInput.java
+++ src/main/java/com/test/librarymanagement/domain/input/patron/PatronUpdateInput.java
@@ -1,4 +1,19 @@
 package com.test.librarymanagement.domain.input.patron;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
 public class PatronUpdateInput {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String Address;
+
+    private String phonenumber;
 }
