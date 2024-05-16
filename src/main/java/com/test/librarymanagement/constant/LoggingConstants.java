d61e69423af3a3084705007d67b39027d95d1dcc
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:51:32 -0700

    logging constants

---
+++ src/main/java/com/test/librarymanagement/constant/LoggingConstants.java
@@ -0,0 +1,7 @@
+package com.test.librarymanagement.constant;
+
+public interface LoggingConstants {
+    String X_REQUEST_ID = "X-libraryManagement-RequestID";
+    String X_SESSION_ID = "X-libraryManagement-SessionID";
+    String X_FORMATTED_HEADERS = "X-Formatted-Headers";
+}
