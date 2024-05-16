b877e115ee31dc138bd4e8287418c6383683113c
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-15 20:56:15 -0700

    Patron create input

---
+++ src/main/java/com/test/librarymanagement/domain/input/patron/PatronCreateInput.java
@@ -0,0 +1,11 @@
+package com.test.librarymanagement.domain.input.patron;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class PatronCreateInput {
+}
