bab0a3cf280a3e2f5f46e2225a1428b272beca2f
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-17 16:18:48 -0700

    refactored

--- src/main/java/com/test/librarymanagement/domain/input/user/UserCreateInput.java
+++ src/main/java/com/test/librarymanagement/domain/input/user/UserCreateInput.java
@@ -1,9 +1,7 @@
 package com.test.librarymanagement.domain.input.user;
 
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
