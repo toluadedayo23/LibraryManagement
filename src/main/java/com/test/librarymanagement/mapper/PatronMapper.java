b71b98cd216e5c128b1f459873e283a7906762ad
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-17 00:30:09 -0700

    added new mapping methods

--- src/main/java/com/test/librarymanagement/mapper/PatronMapper.java
+++ src/main/java/com/test/librarymanagement/mapper/PatronMapper.java
@@ -1,9 +1,12 @@
 package com.test.librarymanagement.mapper;
 
 import com.test.librarymanagement.domain.data.Patron;
+import com.test.librarymanagement.domain.dto.PageableDTO;
 import com.test.librarymanagement.domain.dto.PatronDTO;
 import com.test.librarymanagement.domain.input.patron.PatronCreateInput;
-import org.mapstruct.Mapper;
+import com.test.librarymanagement.domain.input.patron.PatronUpdateInput;
+import org.mapstruct.*;
+import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
 public interface PatronMapper {
@@ -11,5 +14,11 @@ public interface PatronMapper {
     Patron mapInputToPatron(PatronCreateInput input);
 
     PatronDTO mapPatronToDTO(Patron patron);
+
+    @Mapping(target = "pageNumber", source = "number")
+    PageableDTO<PatronDTO> mapToPageableDTO(Page<Patron> result);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void patronUpdateInputToPatron(PatronUpdateInput updateInput, @MappingTarget Patron patron);
 
 }
