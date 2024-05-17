d83fb8227d7c02a0d5c170378e4511c11b4e690c
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-17 16:08:12 -0700

    implements Serializable

--- src/main/java/com/test/librarymanagement/domain/dto/BookDTO.java
+++ src/main/java/com/test/librarymanagement/domain/dto/BookDTO.java
@@ -5,10 +5,14 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import java.io.Serializable;
+
 @Data
 @SuperBuilder
 @NoArgsConstructor
-public class BookDTO {
+public class BookDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
 
     private Long id;
 
