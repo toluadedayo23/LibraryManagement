e51efd3aaf8ca1b93a9cac3d54aa0e1b8bcb9307
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-17 00:27:58 -0700

    repo methods for BorrowingRecord entity

--- src/main/java/com/test/librarymanagement/repository/BorrowingRecordRepository.java
+++ src/main/java/com/test/librarymanagement/repository/BorrowingRecordRepository.java
@@ -1,7 +1,12 @@
 package com.test.librarymanagement.repository;
 
 import com.test.librarymanagement.domain.data.BorrowingRecord;
+import com.test.librarymanagement.domain.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BorrowingRecordRepository extends JpaRepository<BorrowingRecord, Long> {
+
+    Optional<BorrowingRecord> findByBookAndPatronAndStatus(Long book, Long patron, Status status);
 }
