9fbd7f106d525cc57f3d7c82068bf6e2fb2316b8
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-17 16:19:27 -0700

    refactored

--- src/main/java/com/test/librarymanagement/service/UserService.java
+++ src/main/java/com/test/librarymanagement/service/UserService.java
@@ -1,13 +1,8 @@
 package com.test.librarymanagement.service;
 
 import com.test.librarymanagement.domain.data.User;
-import com.test.librarymanagement.domain.dto.UserDTO;
-import com.test.librarymanagement.domain.enums.Role;
-import com.test.librarymanagement.domain.input.user.UserCreateInput;
-import com.test.librarymanagement.mapper.UserMapper;
 import com.test.librarymanagement.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,28 +13,12 @@ import java.util.NoSuchElementException;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
 
 
     public User loadByUsername(String username){
         return userRepository.findByUsername(username)
                 .orElseThrow(NoSuchElementException::new);
-    }
-
-    public UserDTO createLibrarian(UserCreateInput userCreateInput){
-        User user = userMapper.mapToUser(userCreateInput);
-        String password = RandomStringUtils.randomAlphanumeric(10);
-        user.setPassword(passwordEncoder.encode(password));
-        user.setEnabled(true);
-        user.setRole(Role.LIBRARIAN);
-
-        // if email service is enabled, generate random password
-        // for each librarian and force them to change it to personal
-        // password on first time login.
-        // or else cannot use the application
-
-        return userMapper.mapToDTO(userRepository.save(user));
     }
 
 
