cfc32531bee72efcca3a8e59c4bc9e9130dc568a
toluadedayo23 <toluadedayo23@gmail.com>
2024-05-17 00:25:05 -0700

    added some mapping methods

--- src/main/java/com/test/librarymanagement/mapper/BookMapper.java
+++ src/main/java/com/test/librarymanagement/mapper/BookMapper.java
@@ -2,8 +2,11 @@ package com.test.librarymanagement.mapper;
 
 import com.test.librarymanagement.domain.data.Book;
 import com.test.librarymanagement.domain.dto.BookDTO;
+import com.test.librarymanagement.domain.dto.PageableDTO;
 import com.test.librarymanagement.domain.input.book.BookCreateInput;
-import org.mapstruct.Mapper;
+import com.test.librarymanagement.domain.input.book.BookUpdateInput;
+import org.mapstruct.*;
+import org.springframework.data.domain.Page;
 
 @Mapper(componentModel = "spring")
 public interface BookMapper {
@@ -11,4 +14,12 @@ public interface BookMapper {
     Book mapInputToBook(BookCreateInput input);
 
     BookDTO mapBookToDTO(Book book);
+
+    @Mapping(target = "pageNumber", source = "number")
+    PageableDTO<BookDTO> mapToPageableDTO(Page<Book> result);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void bookUpdateInputToBook(BookUpdateInput updateInput, @MappingTarget Book book);
+
+    BookCreateInput mapBookToCreateInput(Book book);
 }
